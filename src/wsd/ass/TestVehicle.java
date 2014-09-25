e45bd3ee91b646b6edecd6f27bb97dd1c3568f43
Trungthi (Calvin) Bui <trbui@au1.ibm.com>
2014-09-25 18:47:29 +1000

    Vehicle should be finished.

---
+++ src/wsd/ass/TestVehicle.java
@@ -0,0 +1,20 @@
+package wsd.ass;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+
+public class TestVehicle {
+	
+	public static void main(String[] args) throws JAXBException {
+		Vehicles vehicles = new Vehicles();
+		vehicles.addVehicle(new Vehicle("BA30HS", "Car", "Toyota", "Corolla", 2009, "Grey", 60000));
+		vehicles.addVehicle(new Vehicle("AAAAAA", "Bus", "Mazda", "3", 2010, "Purple", 1111111));
+		// Boilerplate code to convert objects to XML...
+	    JAXBContext jc = JAXBContext.newInstance(Vehicles.class);
+	    Marshaller m = jc.createMarshaller();
+	    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+	    m.marshal(vehicles, System.out);
+	}
+}
