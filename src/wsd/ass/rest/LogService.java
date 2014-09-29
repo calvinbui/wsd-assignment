fcba881d1e301ebce4c716b0f524aeb9a4f32102
Trungthi (Calvin) Bui <trbui@au1.ibm.com>
2014-09-29 18:38:52 +1000

    fixed exception errors

--- src/wsd/ass/rest/LogService.java
+++ src/wsd/ass/rest/LogService.java
@@ -13,25 +13,28 @@ import wsd.ass.Logs;
 public class LogService {
 	@Context
 	private ServletContext application;
-	
-	private Logs getLogss() {
-		
-		  // The web server can handle requests from different clients in parallel.
-		  // These are called "threads".
-		  //
-		  // We do NOT want other threads to manipulate the application object at the same
-		  // time that we are manipulating it, otherwise bad things could happen.
-		  //
-		  // The "synchronized" keyword is used to lock the application object while
-		  // we're manpulating it.
-		  synchronized (application) {
-		   Logs logs = (Logs)application.getAttribute("logs");
-		   if (logs == null) {
-		    logs = new Logs();
-		    logs.setFilePath(application.getRealPath("logs.xml"));
-		    application.setAttribute("logs", logs);
-		   }
-		   return logs;
-		  }
-		 }
+
+	private Logs getLogss() throws JAXBException, IOException {
+
+		// The web server can handle requests from different clients in
+		// parallel.
+		// These are called "threads".
+		//
+		// We do NOT want other threads to manipulate the application object at
+		// the same
+		// time that we are manipulating it, otherwise bad things could happen.
+		//
+		// The "synchronized" keyword is used to lock the application object
+		// while
+		// we're manpulating it.
+		synchronized (application) {
+			Logs logs = (Logs) application.getAttribute("logs");
+			if (logs == null) {
+				logs = new Logs();
+				logs.setFilePath(application.getRealPath("logs.xml"));
+				application.setAttribute("logs", logs);
+			}
+			return logs;
+		}
+	}
 }
