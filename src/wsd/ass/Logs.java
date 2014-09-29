e89206e5cc73fa64288c15b6159b109120ee30a7
Trungthi (Calvin) Bui <trbui@au1.ibm.com>
2014-09-29 18:38:19 +1000

    Get filepath

--- src/wsd/ass/Logs.java
+++ src/wsd/ass/Logs.java
@@ -1,7 +1,11 @@
 package wsd.ass;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.*;
-
 import java.util.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -12,6 +16,8 @@ public class Logs implements Serializable {
 	 */
 	@XmlElement(name = "log")
 	private ArrayList<Log> logs = new ArrayList<Log>();
+	
+	private String filePath;
 
 	public ArrayList<Log> getLogs() {
         return logs;
@@ -24,5 +30,16 @@ public class Logs implements Serializable {
     public void removeLog(Log log) {
         logs.remove(log);
     }
+    
+    public void setFilePath(String filePath) throws JAXBException, IOException {
+        this.filePath = filePath;
+        // Create the unmarshaller
+        JAXBContext jc = JAXBContext.newInstance(Logs.class);
+        Unmarshaller u = jc.createUnmarshaller();
 
+        // Now unmarshal the object from the file
+        FileInputStream fin = new FileInputStream(filePath);
+        logs = (ArrayList<Log>)u.unmarshal(fin); // This loads the "shop" object
+        fin.close();
+    }
 }
