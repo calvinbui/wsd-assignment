3c10214d009de54862c989980574e24c322ab09f
Trungthi (Calvin) Bui <trbui@au1.ibm.com>
2014-10-14 08:37:50 +1100

    SOAP started

--- src/wsd/ass/soap/LogSOAP.java
+++ src/wsd/ass/soap/LogSOAP.java
@@ -1,5 +1,37 @@
 package wsd.ass.soap;
 
-public class LogSOAP {
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.servlet.ServletContext;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 
+import wsd.ass.LogApplication;
+import wsd.ass.Logs;
+
+@WebService
+public class LogSOAP {
+	@Resource
+	private WebServiceContext context;
+	
+	@WebMethod
+	private LogApplication getLogApp() {
+		ServletContext application = (ServletContext)context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+		synchronized (application) {
+			LogApplication logApp = (LogApplication)application.getAttribute("logApp");
+			if (logApp == null) {
+				logApp = new LogApplication();
+				String filePath = application.getRealPath("WEB-INF/logs.xml");
+				logApp.setFilePath(filePath);
+				application.setAttribute("logApp", logApp);
+			}
+			return logApp;
+		}
+	}
+	
+	@WebMethod
+	public Logs getLogs() {
+		return getLogApp().getLogs();
+	}
 }
