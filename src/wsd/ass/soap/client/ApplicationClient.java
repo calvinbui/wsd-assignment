4166630396833f054912cc86fcb3c8182ef53525
Trungthi (Calvin) Bui <trbui@au1.ibm.com>
2014-10-14 13:41:35 +1100

    SOAP is broken. But at least ApplicationClient is started

--- src/wsd/ass/soap/client/ApplicationClient.java
+++ src/wsd/ass/soap/client/ApplicationClient.java
@@ -1,5 +1,46 @@
 package wsd.ass.soap.client;
 
-public class ApplicationClient {
+import java.rmi.RemoteException;
+import java.util.Scanner;
 
+import javax.xml.rpc.ServiceException;
+
+public class ApplicationClient {
+	private static User user;
+	
+	public static void main(String[] args) throws ServiceException, RemoteException{
+		System.out.println("== Welcome to the Vehicle Log Book System ==");
+		login();
+		
+		LogSOAPServiceLocator logLocator = new LogSOAPServiceLocator();
+		LogSOAP log = logLocator.getLogSOAPPort();
+		
+	}
+	
+	private static void login() throws ServiceException, RemoteException {
+		UserSOAPServiceLocator userLocator = new UserSOAPServiceLocator();
+		UserSOAP userSOAP = userLocator.getUserSOAPPort();
+		
+		boolean userValidated = false;
+		
+		do {
+			System.out.print("Enter email: ");
+			Scanner s = new Scanner(System.in);
+			String email = s.nextLine();
+			System.out.print("Enter password: ");
+			String password = s.nextLine();
+			user = getUser(userSOAP, email, password);
+			if (user != null)
+				userValidated = true;
+			else
+				System.out.println("Login Failed. Try again.");
+			
+		} while (userValidated = false);
+		
+	}
+	
+	private static User getUser(UserSOAP user, String email, String password) throws RemoteException {
+		return user.getUser(email, password);
+	}
+	
 }
