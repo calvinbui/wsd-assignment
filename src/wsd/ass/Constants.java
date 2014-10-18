a535e45db58b7212ef198c13f3c89c58dd855e56
Trungthi (Calvin) Bui <trbui@au1.ibm.com>
2014-10-19 09:56:00 +1100

    Created Constants class to hold filepaths to XML files

---
+++ src/wsd/ass/Constants.java
@@ -0,0 +1,8 @@
+package wsd.ass;
+
+
+public final class Constants {
+	public static final String LOG_XML = "/log.xml";
+	public static final String USER_XML = "/user.xml";
+	public static final String VEHICLE_XML = "/vehicle.xml";
+}
